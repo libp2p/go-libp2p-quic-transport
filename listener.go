05430963bd53b109d7ff56a09a6a51cc3bb5aadb
Marten Seemann <martenseemann@gmail.com>
2017-03-15 19:41:08 +0700

    implement the listener

--- listener.go
+++ listener.go
@@ -1,28 +1,61 @@
 package libp2pquic
 
 import (
+	"crypto/tls"
 	"net"
 
+	pstore "github.com/libp2p/go-libp2p-peerstore"
 	tpt "github.com/libp2p/go-libp2p-transport"
+	quicconn "github.com/marten-seemann/quic-conn"
 	ma "github.com/multiformats/go-multiaddr"
+	manet "github.com/multiformats/go-multiaddr-net"
 )
 
-type listener struct{}
+type listener struct {
+	laddr        ma.Multiaddr
+	quicListener net.Listener
+
+	transport tpt.Transport
+}
+
+func newListener(laddr ma.Multiaddr, peers pstore.Peerstore, transport tpt.Transport) (*listener, error) {
+	tlsConf := &tls.Config{}
+	network, host, err := manet.DialArgs(laddr)
+	if err != nil {
+		return nil, err
+	}
+	qln, err := quicconn.Listen(network, host, tlsConf)
+	if err != nil {
+		return nil, err
+	}
+	return &listener{
+		laddr:        laddr,
+		quicListener: qln,
+		transport:    transport,
+	}, nil
+}
 
 func (l *listener) Accept() (tpt.Conn, error) {
-	panic("not implemented")
+	c, err := l.quicListener.Accept()
+	if err != nil {
+		return nil, err
+	}
+	return &conn{
+		quicConn:  c,
+		transport: l.transport,
+	}, nil
 }
 
 func (l *listener) Close() error {
-	panic("not implemented")
+	return l.quicListener.Close()
 }
 
 func (l *listener) Addr() net.Addr {
-	panic("not implemented")
+	return l.quicListener.Addr()
 }
 
 func (l *listener) Multiaddr() ma.Multiaddr {
-	panic("not implemented")
+	return l.laddr
 }
 
 var _ tpt.Listener = &listener{}
--- transport.go
+++ transport.go
@@ -1,22 +1,53 @@
 package libp2pquic
 
 import (
+	"sync"
+
+	pstore "github.com/libp2p/go-libp2p-peerstore"
 	tpt "github.com/libp2p/go-libp2p-transport"
 	ma "github.com/multiformats/go-multiaddr"
 )
 
-type QuicTransport struct{}
+// QuicTransport implements a QUIC Transport
+type QuicTransport struct {
+	mutex sync.Mutex
 
-func NewQuicTransport() *QuicTransport {
-	return &QuicTransport{}
+	peers pstore.Peerstore
+
+	listeners map[string]tpt.Listener
+}
+
+// NewQuicTransport creates a new QUIC Transport
+// it tracks dialers and listeners created
+func NewQuicTransport(peers pstore.Peerstore) *QuicTransport {
+	return &QuicTransport{
+		peers:     peers,
+		listeners: make(map[string]tpt.Listener),
+	}
 }
 
 func (t *QuicTransport) Dialer(laddr ma.Multiaddr, opts ...tpt.DialOpt) (tpt.Dialer, error) {
 	panic("not implemented")
 }
 
+// Listen starts listening on laddr
 func (t *QuicTransport) Listen(laddr ma.Multiaddr) (tpt.Listener, error) {
-	panic("not implemented")
+	// TODO: check if laddr is actually a QUIC address
+	t.mutex.Lock()
+	defer t.mutex.Unlock()
+
+	l, ok := t.listeners[laddr.String()]
+	if ok {
+		return l, nil
+	}
+
+	ln, err := newListener(laddr, t.peers, t)
+	if err != nil {
+		return nil, err
+	}
+
+	t.listeners[laddr.String()] = ln
+	return ln, nil
 }
 
 func (t *QuicTransport) Matches(ma.Multiaddr) bool {
