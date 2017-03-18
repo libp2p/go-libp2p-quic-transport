0b44affe4091edb6d01b6bfa26892e3c27c6c7cb
Marten Seemann <martenseemann@gmail.com>
2017-03-18 23:06:50 +0700

    add tests for transport.Listen

--- transport_test.go
+++ transport_test.go
@@ -14,6 +14,28 @@ var _ = Describe("Transport", func() {
 		t = NewQuicTransport(nil)
 	})
 
+	Context("listening", func() {
+		It("creates a new listener", func() {
+			maddr, err := ma.NewMultiaddr("/ip4/127.0.0.1/udp/1234/quic")
+			Expect(err).ToNot(HaveOccurred())
+			ln, err := t.Listen(maddr)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(ln.Multiaddr()).To(Equal(maddr))
+		})
+
+		It("returns an existing listener", func() {
+			maddr, err := ma.NewMultiaddr("/ip4/127.0.0.1/udp/1235/quic")
+			Expect(err).ToNot(HaveOccurred())
+			ln, err := t.Listen(maddr)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(ln.Multiaddr()).To(Equal(maddr))
+			ln2, err := t.Listen(maddr)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(ln2).To(Equal(ln))
+			Expect(t.listeners).To(HaveLen(1))
+		})
+	})
+
 	It("matches", func() {
 		invalidAddr, err := ma.NewMultiaddr("/ip4/127.0.0.1/udp/1234")
 		Expect(err).ToNot(HaveOccurred())
