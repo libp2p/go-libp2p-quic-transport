5237396ac707e0b07e2cdaa352dc694e33f508af
Steven Allen <steven@stebalien.com>
2019-11-15 05:19:20 -0800

    test: libp2p transport test suite

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/ipfs/go-log v1.0.4
 	github.com/klauspost/compress v1.11.7
 	github.com/libp2p/go-libp2p-core v0.8.5
+	github.com/libp2p/go-libp2p-testing v0.4.2
 	github.com/libp2p/go-libp2p-tls v0.1.3
 	github.com/libp2p/go-netroute v0.1.3
 	github.com/lucas-clemente/quic-go v0.21.2
---
+++ libp2p_transport_test.go
@@ -0,0 +1,43 @@
+package libp2pquic_test
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"testing"
+
+	ic "github.com/libp2p/go-libp2p-core/crypto"
+	"github.com/libp2p/go-libp2p-core/peer"
+	tpt "github.com/libp2p/go-libp2p-core/transport"
+	quic "github.com/libp2p/go-libp2p-quic-transport"
+	tptt "github.com/libp2p/go-libp2p-testing/suites/transport"
+)
+
+func TestLibp2pTransportSuite(t *testing.T) {
+	create := func() (tpt.Transport, peer.ID, ic.PrivKey) {
+		key, err := rsa.GenerateKey(rand.Reader, 2048)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		priv, err := ic.UnmarshalRsaPrivateKey(x509.MarshalPKCS1PrivateKey(key))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		id, err := peer.IDFromPrivateKey(priv)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		transport, err := quic.NewTransport(priv, nil, nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		return transport, id, priv
+	}
+	clientTpt, _, _ := create()
+	serverTpt, serverID, _ := create()
+
+	tptt.SubtestTransport(t, serverTpt, clientTpt, "/ip4/127.0.0.1/udp/0/quic", serverID)
+}
