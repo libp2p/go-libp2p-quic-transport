059e632d0afcdc03457a430716f74f12ee41407e
Marten Seemann <martenseemann@gmail.com>
2020-04-20 17:16:28 +0700

    add command line client and server

---
+++ cmd/client/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"context"
+	"crypto/rand"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	ic "github.com/libp2p/go-libp2p-core/crypto"
+	peer "github.com/libp2p/go-libp2p-core/peer"
+	libp2pquic "github.com/libp2p/go-libp2p-quic-transport"
+	ma "github.com/multiformats/go-multiaddr"
+)
+
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Printf("Usage: ./main <multiaddr> <peer id>")
+		return
+	}
+	if err := run(os.Args[1], os.Args[2]); err != nil {
+		log.Fatalf(err.Error())
+	}
+}
+
+func run(raddr string, p string) error {
+	peerID, err := peer.Decode(p)
+	if err != nil {
+		return err
+	}
+	addr, err := ma.NewMultiaddr(raddr)
+	if err != nil {
+		return err
+	}
+	priv, _, err := ic.GenerateECDSAKeyPair(rand.Reader)
+	if err != nil {
+		return err
+	}
+
+	t, err := libp2pquic.NewTransport(priv, nil, nil)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("Dialing %s\n", addr.String())
+	conn, err := t.Dial(context.Background(), addr, peerID)
+	if err != nil {
+		return err
+	}
+	str, err := conn.OpenStream()
+	if err != nil {
+		return err
+	}
+	const msg = "Hello world!"
+	log.Printf("Sending: %s\n", msg)
+	if _, err := str.Write([]byte(msg)); err != nil {
+		return err
+	}
+	if err := str.Close(); err != nil {
+		return err
+	}
+	data, err := ioutil.ReadAll(str)
+	if err != nil {
+		return err
+	}
+	log.Printf("Received: %s\n", data)
+	return conn.Close()
+}
