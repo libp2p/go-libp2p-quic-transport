0b9d47ec3d51e1e19dcd2cf3787700945611f098
Marten Seemann <martenseemann@gmail.com>
2022-01-03 19:07:13 +0400

    migrate the tracer tests away from Ginkgo

--- tracer_test.go
+++ tracer_test.go
@@ -2,81 +2,73 @@ package libp2pquic
 
 import (
 	"bytes"
-	"fmt"
 	"io/ioutil"
 	"os"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
 
 	"github.com/klauspost/compress/zstd"
 
 	"github.com/lucas-clemente/quic-go/logging"
-
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("qlogger", func() {
-	var qlogDir string
+func createLogDir(t *testing.T) string {
+	dir, err := ioutil.TempDir("", "libp2p-quic-transport-test")
+	require.NoError(t, err)
+	t.Cleanup(func() { os.RemoveAll(dir) })
+	return dir
+}
 
-	BeforeEach(func() {
-		var err error
-		qlogDir, err = ioutil.TempDir("", "libp2p-quic-transport-test")
-		Expect(err).ToNot(HaveOccurred())
-		fmt.Fprintf(GinkgoWriter, "Creating temporary directory: %s\n", qlogDir)
-		initQlogger(qlogDir)
-	})
+func getFile(t *testing.T, dir string) os.FileInfo {
+	files, err := ioutil.ReadDir(dir)
+	require.NoError(t, err)
+	require.Len(t, files, 1)
+	return files[0]
+}
 
-	AfterEach(func() {
-		Expect(os.RemoveAll(qlogDir)).To(Succeed())
-	})
+func TestSaveQlog(t *testing.T) {
+	qlogDir := createLogDir(t)
+	logger := newQlogger(qlogDir, logging.PerspectiveServer, []byte{0xde, 0xad, 0xbe, 0xef})
+	file := getFile(t, qlogDir)
+	require.Equal(t, string(file.Name()[0]), ".")
+	require.Truef(t, strings.HasSuffix(file.Name(), ".qlog.swp"), "expected %s to have the .qlog.swp file ending", file.Name())
+	// close the logger. This should move the file.
+	require.NoError(t, logger.Close())
+	file = getFile(t, qlogDir)
+	require.NotEqual(t, string(file.Name()[0]), ".")
+	require.Truef(t, strings.HasSuffix(file.Name(), ".qlog.zst"), "expected %s to have the .qlog.zst file ending", file.Name())
+	require.Contains(t, file.Name(), "server")
+	require.Contains(t, file.Name(), "deadbeef")
+}
 
-	getFile := func() os.FileInfo {
-		files, err := ioutil.ReadDir(qlogDir)
-		Expect(err).ToNot(HaveOccurred())
-		Expect(files).To(HaveLen(1))
-		return files[0]
-	}
+func TestQlogBuffering(t *testing.T) {
+	qlogDir := createLogDir(t)
+	logger := newQlogger(qlogDir, logging.PerspectiveServer, []byte("connid"))
+	initialSize := getFile(t, qlogDir).Size()
+	// Do a small write.
+	// Since the writter is buffered, this should not be written to disk yet.
+	logger.Write([]byte("foobar"))
+	require.Equal(t, getFile(t, qlogDir).Size(), initialSize)
+	// Close the logger. This should flush the buffer to disk.
+	require.NoError(t, logger.Close())
+	finalSize := getFile(t, qlogDir).Size()
+	t.Logf("initial log file size: %d, final log file size: %d\n", initialSize, finalSize)
+	require.Greater(t, finalSize, initialSize)
+}
 
-	It("saves a qlog", func() {
-		logger := newQlogger(qlogDir, logging.PerspectiveServer, []byte{0xde, 0xad, 0xbe, 0xef})
-		file := getFile()
-		Expect(string(file.Name()[0])).To(Equal("."))
-		Expect(file.Name()).To(HaveSuffix(".qlog.swp"))
-		// close the logger. This should move the file.
-		Expect(logger.Close()).To(Succeed())
-		file = getFile()
-		Expect(string(file.Name()[0])).ToNot(Equal("."))
-		Expect(file.Name()).To(HaveSuffix(".qlog.zst"))
-		Expect(file.Name()).To(And(
-			ContainSubstring("server"),
-			ContainSubstring("deadbeef"),
-		))
-	})
-
-	It("buffers", func() {
-		logger := newQlogger(qlogDir, logging.PerspectiveServer, []byte("connid"))
-		initialSize := getFile().Size()
-		// Do a small write.
-		// Since the writter is buffered, this should not be written to disk yet.
-		logger.Write([]byte("foobar"))
-		Expect(getFile().Size()).To(Equal(initialSize))
-		// Close the logger. This should flush the buffer to disk.
-		Expect(logger.Close()).To(Succeed())
-		finalSize := getFile().Size()
-		fmt.Fprintf(GinkgoWriter, "initial log file size: %d, final log file size: %d\n", initialSize, finalSize)
-		Expect(finalSize).To(BeNumerically(">", initialSize))
-	})
-
-	It("compresses", func() {
-		logger := newQlogger(qlogDir, logging.PerspectiveServer, []byte("connid"))
-		logger.Write([]byte("foobar"))
-		Expect(logger.Close()).To(Succeed())
-		compressed, err := ioutil.ReadFile(qlogDir + "/" + getFile().Name())
-		Expect(err).ToNot(HaveOccurred())
-		Expect(compressed).ToNot(Equal("foobar"))
-		c, err := zstd.NewReader(bytes.NewReader(compressed))
-		Expect(err).ToNot(HaveOccurred())
-		data, err := ioutil.ReadAll(c)
-		Expect(err).ToNot(HaveOccurred())
-		Expect(data).To(Equal([]byte("foobar")))
-	})
-})
+func TestQlogCompression(t *testing.T) {
+	qlogDir := createLogDir(t)
+	logger := newQlogger(qlogDir, logging.PerspectiveServer, []byte("connid"))
+	logger.Write([]byte("foobar"))
+	require.NoError(t, logger.Close())
+	compressed, err := ioutil.ReadFile(qlogDir + "/" + getFile(t, qlogDir).Name())
+	require.NoError(t, err)
+	require.NotEqual(t, compressed, "foobar")
+	c, err := zstd.NewReader(bytes.NewReader(compressed))
+	require.NoError(t, err)
+	data, err := ioutil.ReadAll(c)
+	require.NoError(t, err)
+	require.Equal(t, data, []byte("foobar"))
+}
